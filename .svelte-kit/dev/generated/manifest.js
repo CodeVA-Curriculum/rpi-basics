8431cb68402a1f778dbbf307c24ef337efa277c8
Jon Stapleton <stapleton.jon.michael@gmail.com>
2021-08-13 13:10:25 -0400

    Initial commit

---
+++ .svelte-kit/dev/generated/manifest.js
@@ -0,0 +1,15 @@
+const c = [
+	() => import("../components/layout.svelte"),
+	() => import("../components/error.svelte"),
+	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/modules/__layout.svelte")
+];
+
+const d = decodeURIComponent;
+
+export const routes = [
+	// src/routes/index.svelte
+	[/^\/$/, [c[0], c[2]], [c[1]]]
+];
+
+export const fallback = [c[0](), c[1]()];
